12ab42727bd5f4f1cfb8e3cca95d0bc1fee2ea39
randocal <rdsoundguy@gmail.com>
2022-12-21 15:06:17 -0500

    cleanmem program is done

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # assembly-game
+
+FOR LINUX:
+
+Need to use an Assembler for the source code. Used https://dasm-assembler.github.io/ here.
+
+To assemble:
+
+'dasm cleanmem.asm -f3 -v0 -ocart.bin'
+
+Linux won't be able to execute this type of Binary file ---> get emulator for this at https://stella-emu.github.io/ OR Javatari OR 8bitworkshop.com for all online.
+
+Once installed. Run with 'stella' which opens a GUI for the program, goto new bin file and run. You can hit ~ for Debugger.
+
+In Stella with ` top right is RAM and bottom right is ROM. in ROM right column is OpCodes like ldx = a2 ff
+
+With PS point stack if letter is Uppercase it is N negative, n is not-negative. And red means is changed since last compuatation, while white is NOT changed. Upper = set, lower = unset
+
+
+
+Note:
+
+When we power-up or reset the Atari 2600, that sets the RESET pin on the 6507 processor. The 6502 CPU (or 6507 in our case) is designed to always initialize and load the PC register (program counter) with the address that is in inside memory positions $FFFC and $FFFD (two bytes).
---
+++ cleanmem/cleanmem.asm
@@ -0,0 +1,37 @@
+    processor 6502 ; always indent. Using 4 spaces here
+    
+    seg code
+    org $F000 ; start of ROM "cartridge" at F000 address
+
+Start:          ; labels to memory address. NOT indented will go to F000
+    sei         ; Disable interrupts. though 6057 doesn't have interrupts. need
+    cld         ; Disable BCD decimal math mode
+    ldx #$FF    ; Load x register with #$FF
+    txs         ;transfer the X register to the Stack pointer reg. Point to last
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; Body of code below
+; Clear the Page Zero region of memory from $00 to $FF
+; Meaning the entire RAM and alsoe th entire TIA registers
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ; start with FF and decrement to zero
+    lda #0      ; A = 0
+    ldx #$FF    ; X is counter of loop.  X = #$FF  ==>>> So A reg has 0 , X has $FF, and S has $FF
+    sta $FF     ; make sure $FF is zeroed before the loop starts since it Decrements then zeros...
+
+MemLoop:        ; start with $FF and make is a Zero, go down to $FE and make zero, goto $FD and make zero...
+    dex         ; X--  (de)crement X
+    sta $0,X    ; store the value of A inside memory $0 Plus whats in X reg
+    bne MemLoop ; branch if NOT 0 go back to MemLoop, it'll stop branching when X is zero (Z-flag is set)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; FILL the ROM size exactly to 4KB
+; Ending the ROM. Need for all 6502
+;;;;;;;;;;;;;;;;;;;;;;
+
+
+    org $FFFC   ; set origin to $FFFC. Jump to FFFC and add two bytes with address of start and again with two bytes 
+    .word Start ; Reset vector at $FFRC (where th program starts)
+    .word Start ; Interrupt vector at $FFFE (unused in VCS)  Required for every 6502
